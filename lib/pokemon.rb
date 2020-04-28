d4786e17a5017a92709371961e0ef94fac9f565f
Javante Nelson <javantenelson@gmail.com>
2020-04-28 14:54:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(id: nil,name:,type:,db:)
+    @id = id 
+    @name = name 
+    @type = type
+    @db = db 
+  end
+  
+  def self.save#(name,type,db) 
+    
+    sql = "INSERT INTO pokemons (name, type, db) 
+      VALUES (?, ?, ?)"
+      
+    DB[:conn].execute(sql, self.name, self.type, self.db)
+     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemons")[0][0]
+  end 
+  
+  def self.find
+  end 
+  
+  
 end
